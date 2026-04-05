f7b19d6d5326d66e257b37debee941d25ae03646
PauloStauffer <paulo.sstauffer@gmail.com>
2026-04-04 21:42:24 -0300

    Add files via upload

---
+++ mavenproject1/src/main/java/HierarquiadeUsuários/Veterinario.java
@@ -0,0 +1,7 @@
+
+package HierarquiadeUsuários;
+
+
+public class Veterinario {
+    private String nome;
+}
---
+++ mavenproject1/src/main/java/Principal/Principal.java
@@ -0,0 +1,13 @@
+
+package Principal;
+
+import MóduloClínico.Animal;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+    }
+}
